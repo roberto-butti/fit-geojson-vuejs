6ff9a557ebbf956fa277b2adc33988b912a00451
Roberto B <roberto.butti@gmail.com>
2019-09-28 10:59:18 +0200

    Auto-lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,15 @@ module.exports = {
     node: true
   },
   extends: [
-    "plugin:vue/essential",
-    "plugin:prettier/recommended",
-    "@vue/prettier"
+    'plugin:vue/essential',
+    'plugin:prettier/recommended',
+    '@vue/prettier'
   ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   }
-};
+}
