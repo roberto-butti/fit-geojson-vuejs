8d15dcd36b163d9b43358e0e3529002dffdda7d0
Yasio <baszczok@yasio.pl>
2019-10-08 23:22:06 +0200

    add Charts route, add Apexcharts for FIT files

--- src/router.js
+++ src/router.js
@@ -21,6 +21,12 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/charts',
+      name: 'charts',
+      component: () =>
+        import(/* webpackChunkName: "charts" */ './views/Charts.vue')
     }
   ]
 })
